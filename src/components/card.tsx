754d6dcd5894e1028895b0bf75a74b2b4636a439
grafika10 <grafika10@ucione.local>
2023-05-22 20:14:09 +0200

    card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -18,7 +18,7 @@ const Card = () => {
 
         <div className="card__main__icon">
           <div>14</div>
-          <div>Daysy</div>
+          <div>Daysys</div>
         </div>
       </div>
       <hr />
